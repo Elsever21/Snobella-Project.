86461e877faf43658b86857987e06d172be860bb
Elsever21 <esihuseynov57@gmail.com>
2021-09-27 11:11:11 +0400

    test

---
+++ basket.js
@@ -0,0 +1,21 @@
+function change1()
+{
+    document.getElementById(`image`).src="kart2.png";
+}
+function change2()
+{
+    document.getElementById(`image`).src="kart1.png";
+}
+function change4()
+{
+    document.getElementById(`image`).src="kart3.png";
+}
+function change5()
+{
+    document.getElementById(`image`).src="kart8.png";
+}
+function change6()
+{
+    document.getElementById(`image`).src="kart7.png";
+}
+
---
+++ js/carousel.js
@@ -0,0 +1,51 @@
+$('#myCarousel').carousel({
+    interval: 1000
+})
+$('#myCarousell').carousel({
+  interval: 2000
+})
+$('.carousel .carousel-item').each(function() {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
+
+
+try {
+    fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' }))
+    .then(function(response) {
+      //   console.log(response)
+      return true;
+    }).catch(function(e) {
+      var carbonScript = document.createElement("script");
+      carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CK7DKKQU&placement=wwwjqueryscriptnet";
+      carbonScript.id = "_carbonads_js";
+      document.getElementById("carbon-block").appendChild(carbonScript);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
